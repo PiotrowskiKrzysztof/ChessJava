f43cfb20d4caf17facadb4e3ae94164a8a921d97
Daniel <dsadlowski98@gmail.com>
2020-11-08 22:23:07 +0100

    dodanie klasy Bishop

---
+++ src/chess/engine/pieces/Bishop.java
@@ -0,0 +1,69 @@
+package chess.engine.pieces;
+
+import chess.engine.Alliance;
+import chess.engine.board.Board;
+import chess.engine.board.BoardUtils;
+import chess.engine.board.Move;
+import chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Bishop extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9, -7, 7, 9}; // pozycje planszy na których może pojawić się figura
+
+    Bishop(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+        for(final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES)
+        {
+            int candidateDestinationCoordinate = this.piecePosition;
+            while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate))
+            {
+                if(isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset) || isEightColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset))
+                {
+                    break;
+                }
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+                if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate))
+                {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+                    if(!candidateDestinationTile.isTileOccupied()) // jeśli dane miejsce nie jest zajęte...
+                    {
+                        legalMoves.add(new Move()); // ...dodajemy ten ruch do listy możliwych ruchów (nieatakujący)
+                    }
+                    else // jeśli jednak dane miejsce jest zajęte...
+                    {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                        if(this.pieceAlliance != pieceAlliance)
+                        {
+                            legalMoves.add(new Move()); // ...dodajemy ten ruch do listy możliwych ruchów (atakujących)
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset)
+    {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && candidateOffset == -9 || candidateOffset == 7;
+    }
+
+    private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset)
+    {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && candidateOffset == -7 || candidateOffset == 9;
+    }
+}
