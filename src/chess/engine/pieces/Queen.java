b0f4cf0177a9aea68b743811a8b6c0fa3d4d4368
PiotrowskiKrzysztof <krzysiek.p97@gmail.com>
2020-11-10 17:39:51 +0100

    dodanie Queen

---
+++ src/chess/engine/pieces/Queen.java
@@ -0,0 +1,75 @@
+package chess.engine.pieces;
+
+import chess.engine.Alliance;
+import chess.engine.board.Board;
+import chess.engine.board.BoardUtils;
+import chess.engine.board.Move;
+import chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Queen extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9}; // pozycje planszy na których może pojawić się figura
+
+    Queen(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+        for(final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES)
+        {
+            int candidateDestinationCoordinate = this.piecePosition;
+            while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate))
+            {
+                if(isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset) ||
+                        isEightColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset))
+                {
+                    break;
+                }
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+                if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate))
+                {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+                    if(!candidateDestinationTile.isTileOccupied()) // jeśli dane miejsce nie jest zajęte...
+                    {
+                        // ...dodajemy ten ruch do listy możliwych ruchów (nieatakujący)
+                        // tworzymy nowy ruch, który przyjmuje w argumentach szachownice, własną figurę oraz docelowe koordynaty
+                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                    else // jeśli jednak dane miejsce jest zajęte...
+                    {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                        if(this.pieceAlliance != pieceAlliance)
+                        {
+                            // ...dodajemy ten ruch do listy możliwych ruchów (atakujących)
+                            // tworzymy nowy ruch atakujący, który przujmuje w argumentach szachownice, własną firugę, docelowe koordynaty oraz atakowaną figurę stojącą na docelowych koordynatach
+                            legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    //Ograniczenia ruchów figury na granicznych kolumnach
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset)
+    {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -1 || candidateOffset == -9 || candidateOffset == 7);
+    }
+
+    private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset)
+    {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == 1 || candidateOffset == -7 || candidateOffset == 9);
+    }
+}
