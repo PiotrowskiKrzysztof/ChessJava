be33595dd8732eb62265d578f955db54e0c969ce
Daniel <dsadlowski98@gmail.com>
2020-11-11 19:52:35 +0100

    rozszerzenie klasy Pawn

--- src/chess/engine/pieces/Pawn.java
+++ src/chess/engine/pieces/Pawn.java
@@ -4,6 +4,7 @@ import chess.engine.Alliance;
 import chess.engine.board.Board;
 import chess.engine.board.BoardUtils;
 import chess.engine.board.Move;
+import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,7 +14,7 @@ import static chess.engine.board.Move.*;
 
 public class Pawn extends Piece { // KLASA REPREZENTUJĄCA FIGURĘ PIONU
 
-    private final static int[] CANDIDATE_MOVE_COORDINATE = {8, 16}; // 8 pól w szachownicy to jedno miejsce wyżej nad pionkiem
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {8, 16, 7, 9}; // 8 pól w szachownicy to jedno miejsce wyżej nad pionkiem
 
     Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -25,19 +26,19 @@ public class Pawn extends Piece { // KLASA REPREZENTUJĄCA FIGURĘ PIONU
 
         for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE) {
             // this.getPieceAlliance().getDirection() - określa kolor (w którą stronę musi iść figura) dla białych -1, czarnych 1
-            final int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset); // miejsce gdzie figura może wykonać ruch
+            final int candidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * currentCandidateOffset); // miejsce gdzie figura może wykonać ruch
 
             // walidacja koordynatów
             if(!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
                 continue;
             }
 
-            // jeżeli pionek jest przesunięty o 8 pól oraz kwadracik szachownicy (na który chcemy iść) NIE jest zajęty
+            // jeżeli pionek jest przesunięty o 8 pól oraz kwadracik szachownicy (na który chcemy iść) NIE jest zajęty (ruch nieatakujący)
             if(currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
                 // tutaj będzie trzeba zrobić specjalny ruch dla pionka
                 legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
             }
-            // odtworzenie "skoku" pionu z pozycji startowej, sprawdzamy czy jest to pierwszy wykonywany ruch na planszy, czy figury w odpowiednich kolorach znajdują się w odpowiadających im rzędach
+            // odtworzenie "skoku" pionu z pozycji startowej, sprawdzamy czy jest to pierwszy wykonywany ruch na planszy, czy figury w odpowiednich kolorach znajdują się w odpowiadających im rzędach (ruch nieatakujący)
             else if(currentCandidateOffset == 16 && this.isFirstMove() && (BoardUtils.SECOND_ROW[this.piecePosition] && this.getPieceAlliance().isBlack()) || (BoardUtils.SEVENTH_ROW[this.piecePosition] && this.getPieceAlliance().isWhite()))
             {
                 // sprawdzamy czy kwadracik będący nad polem nad którym figura wykonuje skok jest pusty
@@ -47,8 +48,34 @@ public class Pawn extends Piece { // KLASA REPREZENTUJĄCA FIGURĘ PIONU
                     legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
                 }
             }
+            // pierwsze dwa ruchy atakujące: ruch atakujący figury po diagonali
+            // warunek dotyczy niemożliwego wyjścia poza planszę gdy figura znajduje się na brzegu planszy i użytkownik chciałby (niechcący lub celowo) wykonać ruch poza nią
+            else if(currentCandidateOffset == 7 && !((BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite() || (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack()))))
+            {
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied())
+                {
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(this.pieceAlliance != pieceOnCandidate.getPieceAlliance())
+                    {
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+
+            }
+
+            else if(currentCandidateOffset == 9 && !((BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite() || (BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack()))))
+            {
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied())
+                {
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(this.pieceAlliance != pieceOnCandidate.getPieceAlliance())
+                    {
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+            }
         }
 
-        return legalMoves;
+        return ImmutableList.copyOf(legalMoves);
     }
 }
